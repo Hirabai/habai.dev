2153d3fe153535163ec28ea84ec9f976ecdfc900
Hirabai <habai.dev@gmail.com>
2026-03-23 10:06:01 -0700

    decided to just stick with css and webp for background

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css';
 import * as THREE from 'three';
 import { SVGLoader } from 'three/addons/loaders/SVGLoader.js';
 
+
+
 // Window size
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -10,13 +12,4 @@ const height = window.innerHeight;
 const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
 camera.position.z = 1;
 
-
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( width, height );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-
-// Render Stage
-function animate( time ) {
-	renderer.render( scene, camera );
-}
+const scene = new THREE.Scene();
