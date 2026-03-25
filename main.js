834be65c0339c7e48f7721892220d35aa9520da7
Hirabai <habai.dev@gmail.com>
2026-03-25 00:43:47 -0700

    a little bit of work before going to bed

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import './style.css';
 import * as THREE from 'three';
 import { SVGLoader } from 'three/addons/loaders/SVGLoader.js';
-
+import { FontLoader } from 'three/addons/loader/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 
 // Window size
@@ -12,4 +13,15 @@ const height = window.innerHeight;
 const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
 camera.position.z = 1;
 
+// Initializing scene, fonts, and other three.js functions
 const scene = new THREE.Scene();
+const loader = new FontLoader();
+
+
+const font = await loader.loadAsync('./Public/fonts/helvetiker_regular.typeface.json');
+const geometry = new TextGeometry( "habai", {
+  font: font,
+  size: 80,
+  depth: 5,
+  curveSegments: 12
+});
